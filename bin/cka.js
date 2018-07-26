c53a78f6ed7098036d5bd947659a90feae7cc299
vito24 <sunhan@suiyi.com.cn>
2018-07-26 13:58:19 +0800

    update

--- bin/cka.js
+++ bin/cka.js
@@ -5,9 +5,6 @@ const program = require('commander');
 
 program
   .version(require('../package').version, '-v, --version')
-  .description('Fake package manager')
+  .description('cka-cli')
   .command('new [name]', 'create a new project').alias('n')
-  .command('search [query]', 'search with optional query').alias('s')
-  .command('list', 'list packages installed')
-  .command('publish', 'publish the package').alias('p')
   .parse(process.argv);
